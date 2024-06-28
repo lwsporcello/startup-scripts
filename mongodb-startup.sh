2b9c054790d53f37158101a50757b40e4c3a18c2
Steve <79470244+lwsporcello@users.noreply.github.com>
2024-06-28 12:46:18 -0400

    Delete mongodb-startup.sh

--- mongodb-startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install gnupg curl
-curl -fsSL https://pgp.mongodb.com/server-7.0.asc | gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/>
-apt-get update
-apt-get install -y mongodb-org
-systemctl start mongod
-systemctl enable mongod
